984a044f14399885aced315832d1c26a7cc12df3
Bryan <bryan.hobin@hotmail.com>
2023-07-25 15:22:33 +0200

    ✨ F01 Login : Creat login Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import LoginPage from "./components/pages/LoginPage";
 
 function App() {
   //state
@@ -17,6 +18,7 @@ function App() {
 
   //render
   return <>
+    <LoginPage />
     <h1>Bienvenue chez nous !</h1>
     <h2>Connectez-vous</h2>
     <form action="submit" onSubmit={handleSubmit}>

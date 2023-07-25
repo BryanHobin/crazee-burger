61afc174d9734e0e86bf639136c89aab27bb9086
Bryan <bryan.hobin@hotmail.com>
2023-07-25 15:28:29 +0200

    ✨ F01 : LOGIN > Add Title and Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,11 @@
-import { useState } from "react";
 import LoginPage from "./components/pages/LoginPage";
 
 function App() {
-  //state
-  const [prenom, setPrenom] = useState("");
-
-  //compo
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (prenom == "") {
-    } else {
-      alert(`Bonjour ${prenom}`);
-      setPrenom("")
-    }
-  }
-
 
   //render
-  return <>
+  return (<>
     <LoginPage />
-    <h1>Bienvenue chez nous !</h1>
-    <h2>Connectez-vous</h2>
-    <form action="submit" onSubmit={handleSubmit}>
-      <input type="text" value={prenom} onChange={(e) => { setPrenom(e.target.value) }} placeholder="Entrez votre prénom..." /><button >Accéder à votre espace</button>
-    </form>
-  </>
+  </>)
 }
 
 export default App
--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,5 +1,24 @@
+import { useState } from "react";
+
 export default function LoginPage() {
- return (
-  <div>LoginPage</div>
- )
+ //state
+ const [prenom, setPrenom] = useState("");
+
+ //compo
+ const handleSubmit = (event) => {
+  event.preventDefault();
+  alert(`Bonjour ${prenom}`);
+  setPrenom("")
+ }
+
+
+ //render
+ return (<>
+  <h1>Bienvenue chez nous !</h1>
+  <br />
+  <h2>Connectez-vous</h2>
+  <form action="submit" onSubmit={handleSubmit}>
+   <input type="text" value={prenom} onChange={(e) => { setPrenom(e.target.value) }} placeholder="Entrez votre prénom..." required /><button >Accéder à votre espace</button>
+  </form>
+ </>)
 }

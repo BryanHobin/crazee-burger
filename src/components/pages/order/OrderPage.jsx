8d9696c857c200e6908ddd269076b317adb057b2
Bryan <bryan.hobin@hotmail.com>
2023-07-28 14:20:39 +0200

    ✨ F02 : LOGIN > Features w/ username accross pages

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,9 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 export default function OrderPage() {
+ //state
+ const { prenom } = useParams()
+
+ //compo
+
+ //render
+
+
  return (
   <div>
-   <h1>OrderPage</h1>
+   <h1>Bonjour {prenom}</h1>
    <Link to="/">
     <button>Déconnexion</button>
    </Link>

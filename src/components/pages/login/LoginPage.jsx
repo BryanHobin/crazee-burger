caf100d4788e1932a15d9a4d63bdf137c509f733
Bryan <bryan.hobin@hotmail.com>
2023-07-31 15:06:58 +0200

    💄 F03 : LOGIN > Add Styling to the background

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -10,17 +10,32 @@ export default function LoginPage() {
  //compo
 
  //render
- return (<LoginPageStyled>
-  <Logo />
-  <LoginForm />
- </LoginPageStyled>)
+ return (
+  <LoginPageStyled>
+   <Logo />
+   <LoginForm />
+  </LoginPageStyled>)
 }
 
 const LoginPageStyled = styled.div`
-  background: red;
   height: 100vh;
   display: flex;
   align-items: center;
   justify-content: center;
   flex-direction: column;
+
+  &::before{
+   content: "";
+   background:url("/images/burger-background.jpg") rgba(0,0,0,0.7);
+   background-size: cover;
+   background-position: center center;
+   background-blend-mode: darken;
+
+   position: absolute;
+   top: 0;
+   bottom: 0;
+   left: 0;
+   right: 0;
+   z-index: -1;
+  }
 `;

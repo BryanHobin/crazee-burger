2c09694114e4768dc3aff13962a8f6d556dd378a
Bryan <bryan.hobin@hotmail.com>
2023-07-31 15:26:56 +0200

    💄 F03 : LOGIN > Add Logo + LoginForm

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -2,39 +2,66 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./LoginForm.css";
 import styled from "styled-components";
+import { theme } from "../theme";
 
 export default function LoginForm() {
- //state
- const [prenom, setPrenom] = useState("");
- const navigate = useNavigate();
+  //state
+  const [prenom, setPrenom] = useState("");
+  const navigate = useNavigate();
 
- //compo
- const handleSubmit = (event) => {
-  event.preventDefault();
-  setPrenom("");
-  navigate(`order/${prenom}`);
- };
- return (
-  <>
-   <LoginFormStyled action="submit" onSubmit={handleSubmit}>
-    <h1>Bienvenue chez nous !</h1>
-    <br />
-    <h2>Connectez-vous</h2>
-    <input
-     type="text"
-     value={prenom}
-     onChange={(e) => {
-      setPrenom(e.target.value);
-     }}
-     placeholder="Entrez votre prénom"
-     required
-    />
-    <button>Accéder à mon espace</button>
-   </LoginFormStyled>
-  </>
- );
+  //compo
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setPrenom("");
+    navigate(`order/${prenom}`);
+  };
+  return (
+    <>
+      <LoginFormStyled action="submit" onSubmit={handleSubmit}>
+        <h1>Bienvenue chez nous !</h1>
+        <hr />
+        <h2>Connectez-vous</h2>
+        <input
+          type="text"
+          value={prenom}
+          onChange={(e) => {
+            setPrenom(e.target.value);
+          }}
+          placeholder="Entrez votre prénom"
+          required
+        />
+        <button>Accéder à mon espace</button>
+      </LoginFormStyled>
+    </>
+  );
 }
 
 const LoginFormStyled = styled.form`
   background: green;
+  text-align: center;
+  font-family: "Amatic SC", cursive;
+  max-width: 500px;
+  min-width: 400px;
+  margin: 0 auto;
+  padding:2.5rem 2rem;
+  border-radius: 5px;
+
+
+  hr{
+    border:1.5px solid ${theme.colors.primary};
+    margin-bottom: 40px;
+  }
+
+  h1{
+    color: white;
+    font-size: 48px;
+  }
+
+  h2{
+    color: ${theme.colors.greyLight};
+    margin: 20px 10px 10px;
+    font-size: 36px;
+  }
+
+
 `;
--- src/components/reusable-ui/Logo.jsx
+++ src/components/reusable-ui/Logo.jsx
@@ -1,11 +1,39 @@
 import { styled } from "styled-components";
+import { theme } from "../pages/theme";
 
 export default function Logo() {
- return (
-  <LogoStyled>Logo CRAZEE BURGER</LogoStyled>
- )
+  return (
+    <LogoStyled>
+      <h1>CRAZEE</h1>
+      <img src="/images/logo-orange.png" alt="Crazee borger logo" />
+      <h1>BURGER</h1>
+    </LogoStyled>
+  )
 }
 
 const LogoStyled = styled.div`
-  background: purple;
+  color: white;
+  display: flex;
+  align-items: center;
+  transform: scale(2.5);
+
+  h1{
+    display: inline;
+    text-align: center;
+    font-family: "Amatic SC", cursive;
+    letter-spacing: 1.5px;
+    line-height: 1em;
+    font-size: 36px;
+    font-weight: 700;
+    text-transform: uppercase;
+    color: ${theme.colors.primary};
+  }
+
+  img{
+    height: 60px;
+    width: 80px;
+    object-fit: contain;
+    object-position: center;
+    margin: 0 5px;
+  }
 `;

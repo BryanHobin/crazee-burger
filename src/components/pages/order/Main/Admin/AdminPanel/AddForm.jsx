cca307cb95f0607f8577cf5b892bb767d492c964
Bryan <bryan.hobin@hotmail.com>
2023-08-08 10:16:38 +0200

    ♻️ F08 : ADMIN > AddForm > Use Propery Name

--- src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
@@ -3,42 +3,51 @@ import { theme } from '../../../../../../theme';
 import { useContext, useState } from 'react';
 import OrderContext from '../../../../../../context/OrderContext';
 
+const EMPTY_PRODUCT = {
+  id: "",
+  title: "",
+  imageSource: "",
+  price: 0
+}
+
 export default function AddForm() {
 
- const { handleAddProduct } = useContext(OrderContext)
- const [title, setTitle] = useState("")
- const [imageSource, setImageSource] = useState("")
- const [price, setPrice] = useState(0)
+  const { handleAddProduct } = useContext(OrderContext)
+  const [newProduct, setNewProduct] = useState(EMPTY_PRODUCT)
 
 
- const handleSubmit = (event, title, imageSource, price) => {
-  event.preventDefault();
-  const productToAdd = {
-   id: new Date().getTime(),
-   title,
-   imageSource,
-   price
+  const handleSubmit = (event, title, imageSource, price) => {
+    event.preventDefault();
+    const productToAdd = {
+      id: new Date().getTime(),
+      title: newProduct.title,
+      imageSource: newProduct.imageSource,
+      price: newProduct.price
+    }
+    handleAddProduct(productToAdd)
+    setNewProduct(EMPTY_PRODUCT)
   }
-  handleAddProduct(productToAdd)
-  setTitle("")
-  setImageSource("")
-  setPrice("")
- }
 
- return (
-  <AddFormStyled onSubmit={(event) => handleSubmit(event, title, imageSource, price)}>
-   <div className="image-preview">Aucune Image</div>
-   <div className="inputs">
-    <input value={title} onChange={(e) => setTitle(e.target.value)} type="text" placeholder="Nom" required />
-    <input value={imageSource} onChange={(e) => setImageSource(e.target.value)} type="text" placeholder="Image URL" required />
-    <input value={price ? price : ""} onChange={(e) => setPrice(e.target.value)} type="text" placeholder="Prix" required />
-   </div>
-   <div className="submit">
-    <button>Submit</button>
-    <div className="info-box">infoBox</div>
-   </div>
-  </AddFormStyled>
- )
+  const handleChange = (event) => {
+    const newValue = event.target.value
+    const name = event.target.name
+    setNewProduct({ ...newProduct, [name]: newValue })
+  }
+
+  return (
+    <AddFormStyled onSubmit={(event) => handleSubmit(event, title, imageSource, price)}>
+      <div className="image-preview">Aucune Image</div>
+      <div className="inputs">
+        <input name="title" value={newProduct.title} onChange={handleChange} type="text" placeholder="Nom" required />
+        <input name="imageSource" value={newProduct.imageSource} onChange={handleChange} type="text" placeholder="Image URL" required />
+        <input name="price" value={newProduct.price ? newProduct.price : ""} onChange={handleChange} type="text" placeholder="Prix" required />
+      </div>
+      <div className="submit">
+        <button>Submit</button>
+        <div className="info-box">infoBox</div>
+      </div>
+    </AddFormStyled>
+  )
 }
 const AddFormStyled = styled.form`
   border: 1px solid red;

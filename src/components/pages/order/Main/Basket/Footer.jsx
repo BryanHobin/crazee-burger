06ac2b256ff98f7926355a75ba5b7a5395c37623
Bryan <bryan.hobin@hotmail.com>
2023-08-16 11:59:58 +0200

    ✨  F10 : BASKET > Add Footer

--- src/components/pages/order/Main/Basket/Footer.jsx
+++ src/components/pages/order/Main/Basket/Footer.jsx
@@ -1,5 +1,28 @@
+import { styled } from "styled-components";
+import { theme } from "../../../../../theme";
+import Header from "../../../../reusable-ui/Header";
+
 export default function Footer() {
  return (
-  <div>Footer</div>
+  <Header>
+   <FooterStyled>
+    <span>
+     Codé avec ❤️ et React.JS
+    </span>
+   </FooterStyled>
+  </Header>
+
  )
 }
+
+const FooterStyled = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  font-family: "Amatic SC", cursive;
+  color: ${theme.colors.white};
+  font-size: ${theme.fonts.size.P2};
+  font-weight: ${theme.fonts.weights.bold};
+  letter-spacing: 2px;
+`;
